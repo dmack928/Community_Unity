a498cfe102805534efd67fdfa5a99bc5e92d1d0e
Danny Mackey <dmack928@gmail.com>
2018-11-02 14:19:18 -0400

    CRUD complete

--- community_unity_app/app/controllers/messages_controller.rb
+++ community_unity_app/app/controllers/messages_controller.rb
@@ -1,7 +1,11 @@
 class MessagesController < ApplicationController
-
+  before_action :find_message, only: [:show, :edit, :update, :destroy]
 
   def index
+    @messages = Message.all.order("created_at DESC")
+  end
+
+  def show
   end
 
   def new
@@ -10,12 +14,35 @@ class MessagesController < ApplicationController
 
   def create
     @message = Message.new(message_params)
+    if @message.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @message.update(message_params)
+      redirect_to message_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @message.destroy
+    redirect_to root_path
   end
 
   private
-
     def message_params
-      (params.require(:message).permit(:title, :description))
+      params.require(:message).permit(:title, :description)
+    end
 
-end
+    def find_message
+      @message = Message.find(params[:id])
+    end
 end
