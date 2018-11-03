24135cc2bea70b9f53f462d0b0d9ce3c733369db
Danny Mackey <dmack928@gmail.com>
2018-11-02 22:22:38 -0400

    fixed_db

--- community_unity_app/db/schema.rb
+++ community_unity_app/db/schema.rb
@@ -17,14 +17,13 @@ ActiveRecord::Schema.define(version: 2018_11_02_202503) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
-    t.integer "message_id"
-    t.integer "user_id"
+    t.bigint "message_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["message_id"], name: "index_comments_on_message_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
-ActiveRecord::Schema.define(version: 2018_11_02_194917) do
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
@@ -46,4 +45,5 @@ ActiveRecord::Schema.define(version: 2018_11_02_194917) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "users"
 end
