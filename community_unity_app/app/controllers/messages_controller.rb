aaf6554380ba48c85a4c5903f5d0f3a7cbb4807e
Danny Mackey <dmack928@gmail.com>
2018-11-02 22:09:43 -0400

    fixed create

--- community_unity_app/app/controllers/messages_controller.rb
+++ community_unity_app/app/controllers/messages_controller.rb
@@ -13,7 +13,7 @@ class MessagesController < ApplicationController
   end
 
   def create
-    @message = Message.new(message_params)
+    @message = current_user.messages.build(message_params)
     if @message.save
       redirect_to root_path
     else
--- community_unity_app/db/schema.rb
+++ community_unity_app/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_11_02_202503) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.text "content"
     t.integer "message_id"
